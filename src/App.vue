b823f0898a043829442c1189c81826e82bcef539
Patrick Wilson <patrickafriyiewilson12345@gmail.com>
2024-09-10 09:02:06 +0000

    HeroSection and SectionTwo

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HeroSection from '@/components/HeroSection.vue'
+import SectionTwo from '@/components/SectionTwo.vue'
+
+</script>
+
+
+
+<template>
+  <main>
+    <HeroSection.vue />
+    <SectionTwo.vue />
+
+  </main>
+</template>
